fab812eed24f2d21a9f037ae28b3544dd26e01d3
Victor <2417328+vrobinson@users.noreply.github.com>
2025-03-31 20:19:57 +0000

    Add initial build and basic code-coverage test

---
+++ test-dotnet.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+exitStatus=0
+
+# To find new versions of dotnet-reportgenerator-globaltool
+# See: https://www.nuget.org/packages/dotnet-reportgenerator-globaltool
+dotnet tool install --create-manifest-if-needed dotnet-reportgenerator-globaltool --version 5.4.5
+
+# Run tests and generate coverage report
+find . -type f -iwholename "$1" -exec dotnet test {} --configuration Release --collect:"XPlat Code Coverage" --collect:"Code Coverage" --logger:trx --results-directory "covered-test-results/" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=cobertura \;
+exitStatus=$((status + $?))
+
+dotnet reportgenerator -targetdir:./covered-test-results/reports/ -reports:./covered-test-results/**/coverage.cobertura.xml -verbosity:Info -reporttypes:"Badges;MarkdownSummary;MarkdownSummaryGitHub;HtmlInline_AzurePipelines_Dark"
+exitStatus=$((status + $?))
+
+exit $exitStatus
---
+++ tests/code-coverage-test/test-dotnet.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Mock test-dotnet.sh running..."
+echo "Test projects pattern: $1"
