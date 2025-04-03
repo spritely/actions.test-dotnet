b792b11b3e60424d55a2fc7e84c7a228abe2dcfb
Victor <2417328+vrobinson@users.noreply.github.com>
2025-04-03 20:28:22 +0000

    Make unit test path more flexible

--- test-dotnet.sh
+++ test-dotnet.sh
@@ -7,7 +7,7 @@ exitStatus=0
 dotnet tool install --create-manifest-if-needed dotnet-reportgenerator-globaltool --version 5.4.5
 
 # Run tests and generate coverage report
-find . -type f -iwholename "$1" -exec dotnet test {} --configuration Release --collect:"XPlat Code Coverage" --collect:"Code Coverage" --logger:trx --results-directory "covered-test-results/" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=cobertura \;
+find . -type f -iwholename "*$1" -exec dotnet test {} --configuration Release --collect:"XPlat Code Coverage" --collect:"Code Coverage" --logger:trx --results-directory "covered-test-results/" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=cobertura \;
 exitStatus=$((status + $?))
 
 dotnet reportgenerator -targetdir:./covered-test-results/reports/ -reports:./covered-test-results/**/coverage.cobertura.xml -verbosity:Info -reporttypes:"Badges;MarkdownSummary;MarkdownSummaryGitHub;HtmlInline_AzurePipelines_Dark"
