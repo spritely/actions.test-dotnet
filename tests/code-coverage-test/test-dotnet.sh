a9b4776aa95ab367b6f38e86989af8c540233bca
Victor <2417328+vrobinson@users.noreply.github.com>
2025-04-03 20:40:31 +0000

    Revert change to test-dotnet.sh for coverage tests

--- tests/code-coverage-test/test-dotnet.sh
+++ tests/code-coverage-test/test-dotnet.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo "test-dotnet.sh: $@" > /logs/test-dotnet.log
-echo "NUGET_USER: ${NUGET_USER}" >> /logs/test-dotnet.log
-echo "NUGET_TOKEN: ${NUGET_TOKEN}" >> /logs/test-dotnet.log
+echo "Mock test-dotnet.sh running..."
+echo "Test projects pattern: $1"
