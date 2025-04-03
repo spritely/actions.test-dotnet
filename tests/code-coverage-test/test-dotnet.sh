1d3e2019111a513b6731a67c5c5fd406aac6c10c
Victor <2417328+vrobinson@users.noreply.github.com>
2025-04-03 20:34:05 +0000

    Fix bug in mock test-dotnet.sh

--- tests/code-coverage-test/test-dotnet.sh
+++ tests/code-coverage-test/test-dotnet.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Mock test-dotnet.sh running..."
-echo "Test projects pattern: $1"
+echo "test-dotnet.sh: $@" > /logs/test-dotnet.log
+echo "NUGET_USER: ${NUGET_USER}" >> /logs/test-dotnet.log
+echo "NUGET_TOKEN: ${NUGET_TOKEN}" >> /logs/test-dotnet.log
--- tests/devcontainer-test/test-dotnet.sh
+++ tests/devcontainer-test/test-dotnet.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "test-dotnet.sh: $@" > /logs/test-dotnet.log
-echo "NUGET_USER: ${NUGET_USER}" > /logs/test-dotnet.log
-echo "NUGET_TOKEN: ${NUGET_TOKEN}" > /logs/test-dotnet.log
+echo "NUGET_USER: ${NUGET_USER}" >> /logs/test-dotnet.log
+echo "NUGET_TOKEN: ${NUGET_TOKEN}" >> /logs/test-dotnet.log
